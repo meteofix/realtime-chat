5ad0f10575ec5012df844f91c41079390a030588
meteofix <meteofix@gmail.com>
2021-10-12 23:27:03 +0300

    feat: add material navbar and sign buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import AppRouter from "./components/AppRouter";
+import './App.css'
 
 const App = () => {
     return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+import {Button} from "@mui/material";
+import styled from "@mui/material/styles/styled";
+import blue from "@mui/material/colors/blue";
+import {NavLink} from "react-router-dom";
+import {LOGIN_ROUTE} from "../utils/consts";
 
 const Navbar = () => {
+    const ColorButton = styled(Button)(({ theme }) => ({
+        color: theme.palette.getContrastText(blue[700]),
+        backgroundColor: blue[700],
+        '&:hover': {
+            backgroundColor: blue[700],
+        },
+    }));
+    const user = true;
+
     return (
-        <div>
-            Navbar
-        </div>
+            <AppBar position="static">
+                <Toolbar variant="dense">
+                    <Grid container justifyContent={"flex-end"}>
+                        {user ?
+                            <NavLink>
+                                <ColorButton>Sign out</ColorButton>
+                            </NavLink>
+                            :
+                            <NavLink to={LOGIN_ROUTE}>
+                                <ColorButton>Sign in</ColorButton>
+                            </NavLink>
+                        }
+                    </Grid>
+                </Toolbar>
+            </AppBar>
     );
 };
 
