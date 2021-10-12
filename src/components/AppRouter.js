2427f4bf8273d3e9dd5271c576b527304e5d85ee
meteofix <meteofix@gmail.com>
2021-10-12 21:54:01 +0300

    fix(AppRouter): syntactic typo

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -4,22 +4,22 @@ import {privateRoutes, publicRoutes} from "./routes";
 import {CHAT_ROUTE, LOGIN_ROUTE} from "../utils/consts";
 
 const AppRouter = () => {
-    const user = false;
+    const user = true;
     return user ?
         (
             <Switch>
-                {privateRoutes.map(({path, Component}) => {
-                    <Route path={path} component={Component} exact />
-                })};
+                {privateRoutes.map(({path, Component}) =>
+                    <Route path={path} component={Component} exact={true} />
+                )};
                 <Redirect to={CHAT_ROUTE}/>;
             </Switch>
         )
         :
         (
             <Switch>
-                {publicRoutes.map(({path, Component}) => {
-                    <Route path={path} component={Component} exact />
-                })}
+                {publicRoutes.map(({path, Component}) =>
+                    <Route path={path} component={Component} exact={true} />
+                )}
                 <Redirect to={LOGIN_ROUTE}/>
             </Switch>
         )
